2e52c9b556f17bc3cbde8e8ac213f8fee15de191
Mehmet Firat Aksoy <m.firat.aksoy@tcu.edu>
2021-08-26 14:11:38 -0500

    initial
    intiiali

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Template Name: Bikin - v4.3.0
+ * Template URL: https://bootstrapmade.com/bikin-free-simple-landing-page-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+
+// window.addEventListener('load', () => {
+//   AOS.init({
+//     duration: 1000,
+//     easing: 'ease-in-out',
+//     once: true,
+//     mirror: false
+//   });
+// });
+
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
+});
